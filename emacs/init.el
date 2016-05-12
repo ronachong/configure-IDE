a03cd559d838db3ef02e467f988c31db5a04bf5f
Rona Chong <ronachong@gmail.com>
2016-05-12 02:03:12 +0000

    Add further configurations for fci.

--- emacs/init.el
+++ emacs/init.el
@@ -5,8 +5,17 @@
 
 
 ;;; Fill Column Indicator
-;; include Fill Column Indicator lib (see https://github.com/alpaker/Fill-Column-Indicator)
+;; include Fill Column Indicator lib (see https://github.com/alpaker/Fill-Column
+;; -Indicator)
 (require 'fill-column-indicator)
+
+;; set the default fill-column to 80
+(setq-default fill-column 80)
+
+;; this makes default emacs-lisp-mode mode hook into fci-mode
+(add-hook 'emacs-lisp-mode-hook (lambda ()
+    (fci-mode 1)
+  ))
 
 
 ;;; Column Marker
