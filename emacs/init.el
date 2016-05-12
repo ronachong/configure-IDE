359972b60f6add4ff8cece53095dda951d0f25a6
Rona Chong <ronachong@gmail.com>
2016-05-12 19:30:44 +0000

    Add kernel style customizations to emacs.

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,38 @@
+;;; Modifications for kernel coding style
+;; see http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/tree/Docum/
+;; entation/CodingStyle?id=refs/heads/master
+
+(defun c-lineup-arglist-tabs-only (ignored)
+  "Line up argument lists by tabs, not spaces"
+  (let* ((anchor (c-langelem-pos c-syntactic-element))
+         (column (c-langelem-2nd-pos c-syntactic-element))
+         (offset (- (1+ column) anchor))
+         (steps (floor offset c-basic-offset)))
+    (* (max steps 1)
+       c-basic-offset)))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            ;; Add kernel style
+            (c-add-style
+             "linux-tabs-only"
+             '("linux" (c-offsets-alist
+                        (arglist-cont-nonempty
+                         c-lineup-gcc-asm-reg
+                         c-lineup-arglist-tabs-only))))))
+
+(add-hook 'c-mode-hook
+          (lambda ()
+            (let ((filename (buffer-file-name)))
+              ;; Enable kernel mode for the appropriate files
+              (when (and filename
+                         (string-match (expand-file-name "~/src/linux-trees")
+                                       filename))
+                (setq indent-tabs-mode t)
+                (setq show-trailing-whitespace t)
+                (c-set-style "linux-tabs-only")))))
+
+
 ;;; Personal Libraries
 ;; adds 'emacslibs' directory to front of 'load-path' var
 ;; this dir contains lisp libraries for Column Marker, Fill Column Indicator
@@ -23,7 +58,8 @@
 (require 'column-marker)
 
 ;; highlight col 80 in foo mode (not sure what foo mode rly is yet)
-(add-hook 'foo-mode-hook (lambda () (interactive) (column-marker-1 80)))
+(add-hook 'emacs-lisp-mode-hook (lambda ()
+    (interactive) (column-marker-1 80)))
 
 ;; set 'C-c m' to interactively highlight with 'column-marker-1-face'
 (global-set-key [?\C-c ?m] 'column-marker-1)
